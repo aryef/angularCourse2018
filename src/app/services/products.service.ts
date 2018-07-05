1174b80a383e15c026b2729c6d4d13b02d16c512
aryef <arye.friedman@gmail.com>
2018-07-05 13:43:08 +0300

    add http REST remote server call

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Product } from '../models/product';
 import { Observable, Observer } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'  ///makes it global singleton
 })
 export class ProductsService {
  
-
+    public constructor (private httpClent: HttpClient){}
 
   public getTopProducts(): Product[]{
 
@@ -16,6 +17,11 @@ export class ProductsService {
     products.push(new Product(2, "Banana", 2.5,200));
     products.push(new Product(3, "Peach", 4.5,300));
         return products;
+  }
+
+  public getAllProducts(): Observable<Product[]>
+  {
+    return this.httpClent.get<Product[]>("/assets/json/products.json");
   }
 
   public getTopProductsAsync3() : Observable<Product[]> {
