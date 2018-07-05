f0c01bfc1dec71ec458a8ea421a526dc4881671c
aryef <arye.friedman@gmail.com>
2018-07-05 14:14:44 +0300

    FIX BUG IN top products observable service

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -4,50 +4,44 @@ import { Observable, Observer } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'  ///makes it global singleton
+    providedIn: 'root'  ///makes it global singleton
 })
 export class ProductsService {
- 
-    public constructor (private httpClent: HttpClient){}
 
-  public getTopProducts(): Product[]{
+    public constructor(private httpClent: HttpClient) { }
 
-    let products:Product[] =[];
-    products.push(new Product(1, "Apple", 3.5,100));
-    products.push(new Product(2, "Banana", 2.5,200));
-    products.push(new Product(3, "Peach", 4.5,300));
+    public getTopProducts(): Product[] {
+
+        let products: Product[] = [];
+        products.push(new Product(1, "Apple", 3.5, 100));
+        products.push(new Product(2, "Banana", 2.5, 200));
+        products.push(new Product(3, "Peach", 4.5, 300));
         return products;
-  }
+    }
 
-  public getAllProducts(): Observable<Product[]>
-  {
-    return this.httpClent.get<Product[]>("/assets/json/products.json");
-  }
+    public getAllProducts(): Observable<Product[]> {
+        return this.httpClent.get<Product[]>("/assets/json/products.json");
+    }
 
-  public getTopProductsAsync3() : Observable<Product[]> {
-      let observable = Observable.create((observer:Observer<Product[]>) =>
-      setInterval(function()
-      {
-          try{
+    public getTopProductsAsync3(): Observable<Product[]> {
+        let observable = Observable.create((observer: Observer<Product[]>) => {
+            setInterval(function () {
+                try {
 
-            let products:Product[] = new Array<Product>();
-            products.push(new Product(1, "Apple", 3.5,100));
-            products.push(new Product(2, "Banana", 2.5,200));
-            products.push(new Product(3, "Peach", 4.5,300));
-            observer.next(products);
+                    let products: Product[] = new Array<Product>();
+                    products.push(new Product(1, "Apple", 3.5, 100));
+                    products.push(new Product(2, "Banana", 2.5, 200));
+                    products.push(new Product(3, "Peach", 4.5, 300));
+                    observer.next(products);
 
-          }
-          catch(error)
-          {
-            observer.error(error);
-          }
-        }, 2000)
-    );
-    
-          
+                }
+                catch (error) {
+                    observer.error(error);
+                }
+            }, 2000);
+        });
+        return observable;
+    }
 
-          return observable;
-      }
-     
-  
+
 }
