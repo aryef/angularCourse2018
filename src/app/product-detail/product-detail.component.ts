d34e6f94b1b206805b7bd3bf4aef7d7509873aec
aryef <arye.friedman@gmail.com>
2018-07-05 15:40:33 +0300

    receive a parameter to the details page

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-detail',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute) {
+
+
+   }
 
   ngOnInit() {
+      let id: number = parseInt( this.activatedRoute.snapshot.params["id"]);
+      console.log(id);
   }
 
 }
