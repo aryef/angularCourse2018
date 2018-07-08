35fec70992f7203705fd7d072c295468eb908b2e
aryef <arye.friedman@gmail.com>
2018-07-08 09:22:17 +0300

    working event on mouse enter/leave

--- src/app/thumbnail/thumbnail.component.ts
+++ src/app/thumbnail/thumbnail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-thumbnail',
@@ -10,12 +10,32 @@ export class ThumbnailComponent implements OnInit {
     @Input()
     public source:string;
 
+
+
     @Input("picWidth")   //can cnange name of binding if needed
     public picWidth:number;
+    
     @Input()
     public picHeight:number;
+
+    @Output()
+    public enter: EventEmitter<string> = new EventEmitter<string>();
+
+    @Output()
+    public leave: EventEmitter<string> = new EventEmitter<string>();
     
   constructor() { }
+
+  public mouseEnter():void{
+
+    this.enter.emit(this.source); //raising the event
+  }
+
+  public mouseLeave():void{
+
+    this.leave.emit(this.source); //raising the event
+  }
+
 
   ngOnInit() {
   }
